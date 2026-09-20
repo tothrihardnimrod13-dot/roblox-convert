034ab93b75158df9911c4602d7a323ceef342c54
tothrihardnimrod13-dot <tothrihardnimrod13@gmail.com>
2026-09-20 15:32:55 +0300

    convert.luau

--- convert.lua
+++
@@ -1,6 +0,0 @@
-local inputFile = "99_nights_in_the_city_V8_1_ALL_PART_PROPERTIES_TRAFFIC_ALL_REQUESTED_INSTANCES_FIXED.rbxlx"
-local outputFile = "99_nights_in_the_city_V8_1_PLAYABLE_FIXED.rbxl"
-
-local game = remodel.readPlaceFile(inputFile)
-remodel.writePlaceFile(game, outputFile)
-print("Sikeres konvertálás!")
